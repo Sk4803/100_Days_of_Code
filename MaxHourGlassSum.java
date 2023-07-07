1a2f1e4406d32712f91d2ab51e4c2230d95dfc32
Sk4803 <80695246+Sk4803@users.noreply.github.com>
2023-07-07 22:39:37 +0530

    Create MaxHourGlassSum.java

---
+++ MaxHourGlassSum.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int hourglassSum(List<List<Integer>> arr) {
+        int largest=0;
+        int sum=0;
+        ArrayList tempArr=new ArrayList<>((arr.size()-2)*(arr.size()-2));
+        for (int i = 0; i < arr.size()-2; i++) {
+            for (int j = 0; j < arr.size()-2; j++) {
+                sum=arr.get(i).get(j)+arr.get(i).get(j+1)+arr.get(i).get(j+2)+arr.get(i+1).get(j+1)+arr.get(i+2).get(j)+arr.get(i+2).get(j+1)+arr.get(i+2).get(j+2);
+                tempArr.add(sum);
+            }
+        }
+        Collections.sort(tempArr, Collections.reverseOrder()); 
+        largest=(Integer)tempArr.get(0); 
+        return largest;
+    }
+
+}
+
+public class MaxHourGlassSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
