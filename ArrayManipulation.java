f8a95ad68f2a914edc8d4a022509c0f6d7188d16
Sk4803 <80695246+Sk4803@users.noreply.github.com>
2023-07-09 22:58:12 +0530

    Create ArrayManipulation.java

---
+++ ArrayManipulation.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static long arrayManipulation(int n, List<List<Integer>> queries) {
+        long[] arr = new long[n+1];
+        for(int i=0;i<queries.size();i++){       
+            int from =queries.get(i).get(0)-1;            
+            int to =queries.get(i).get(1); 
+            int toAdd =queries.get(i).get(2); 
+            
+            arr[from]+=toAdd;
+            arr[to]-=toAdd;       
+        }
+        long max =0,sum=0;
+         for(long j: arr){         
+            sum += j;
+            max = Math.max( sum,max);
+        }
+ 
+        return max;
+    }
+
+}
+
+public class ArrayManipulation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        IntStream.range(0, m).forEach(i -> {
+            try {
+                queries.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        long result = Result.arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
