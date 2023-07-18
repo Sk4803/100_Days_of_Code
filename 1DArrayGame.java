f9ac4be7099937a91318c369b03b8379e3bf4f7b
Sk4803 <80695246+Sk4803@users.noreply.github.com>
2023-07-18 22:51:09 +0530

    Create 1DArrayGame.java

---
+++ 1DArrayGame.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class 1DArrayGame {
+    public static boolean rec(int leap, int[]  game, int i, boolean[] visited){
+        if(i>=game.length){
+            return true;
+        }
+        if(visited[i] == true || game[i]!=0){
+            return false;
+        }
+        visited[i] = true; 
+        if(rec(leap, game, i+leap, visited)){
+            return true;
+        }
+        if(rec(leap, game, i+1, visited)){
+            return true;
+        }
+        if(i-1>=0){
+            if(rec(leap, game, i-1, visited)){
+                return true;
+            }
+        }       
+        return false;   
+    }
+    
+    public static boolean canWin(int leap, int[] game) {
+        boolean[] visited = new boolean[game.length];
+        int i = 0;
+        return rec(leap, game, i, visited);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+            
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+        }
+    }
+}
