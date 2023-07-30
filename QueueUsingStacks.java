a30d8e9f785322bb286a6c3000902de678d95148
Sk4803 <80695246+Sk4803@users.noreply.github.com>
2023-07-30 22:58:13 +0530

    Update QueueUsingStacks.java

--- QueueUsingStacks.java
+++ QueueUsingStacks.java
@@ -1,52 +1,38 @@
 import java.io.*;
 import java.util.*;
 
-class Queue {
-        static Stack<Integer> s1 = new Stack<Integer>();
-        static Stack<Integer> s2 = new Stack<Integer>();
+public class QueueUsingStacks {
 
-        static void enQueue(int x){
-            while (!s1.isEmpty()){
-                s2.push(s1.pop());
-            }
-
-            s1.push(x);
-            while (!s2.isEmpty()){
-                s1.push(s2.pop());
-            }
-        }
-
-        static int deQueue(){
-            if (s1.isEmpty()){
-                return -1;
-            }
-
-            int x = s1.peek();
-            s1.pop();
-            return x;
-        }
-        
-        static int queueFront(){
-            return s1.peek();
-        }    
-};
-
-public class QueueUsingStacks {   
     public static void main(String[] args) {
+        Stack<Integer> first = new Stack<>();
+        Stack<Integer> second = new Stack<>();
         Scanner sc = new Scanner(System.in);
-        Queue q = new Queue();
-        int n = sc.nextInt();
-        for(int i=0; i<n; i++){
-            int query = sc.nextInt();
-            if(query ==1){
-                q.enQueue(sc.nextInt());
+        Integer n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            switch(num) {
+                case 1:
+                int a = sc.nextInt();
+                first.push(a);
+                break;
+                case 2:
+                case 3:
+                if(second.isEmpty()){   
+                while(!first.isEmpty()) {
+                    second.push(first.pop());
+                }   
+                }
+                switch(num) {
+                    case 2:
+                    second.pop();
+                    break;
+                    case 3:
+                    System.out.println(second.peek());
+                    break;
+                }
+                break;
             }
-            else if(query == 2){
-                q.deQueue();
-            }
-            else if(query == 3){
-                System.out.println(q.queueFront());
-            }
-        }
+        }        
     }
 }
